1b523f96acc9855000843f348ca73534bf4a4158
deni dharmatin <deni.dharmatin@rea-group.com>
2019-08-12 21:17:48 +0700

    Implement config on server

--- app/server/server.go
+++ app/server/server.go
@@ -1,9 +1,8 @@
 package server
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"../../app/config"
@@ -13,13 +12,13 @@ import (
 //Start server
 func Start() {
 	e := echo.New()
-	cfg, configErr := config.LoadEnvConfig("../config/config.json")
-	fmt.Println(configErr)
+	wd, _ := os.Getwd()
+	cfg, configErr := config.LoadEnvConfig(wd + "/app/config/config.json")
 	if configErr != nil {
-		log.Fatal("Can't start server, configuration not found")
+		e.Logger.Fatal("Can't start server, configuration not found")
 	}
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World")
 	})
-	e.Logger.Fatal(e.Start(":" + strconv.Itoa(cfg.Port)))
+	e.Logger.Fatal(e.Start("localhost:" + strconv.Itoa(cfg.Port)))
 }
